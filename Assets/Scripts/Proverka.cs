06077dfe957e20bbe3ef87e354e6dad90ed0829d
DESKTOP-4G3BCO0\DomovenoKuzja <kravcenkovaleria06@gmail.com>
2020-09-08 17:49:46 +0300

    NIK 1.0.0

---
+++ Assets/Scripts/Proverka.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Dominos;
+
+public class Proverka : MonoBehaviour
+{
+    void Start()
+    {
+        Player player = new Player();
+        player.ShowHand();
+    }
+}
