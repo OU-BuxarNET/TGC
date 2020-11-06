6b0a260e403a10eeb8607d918c4070b0cb673b82
DESKTOP-4G3BCO0\DomovenoKuzja <kravcenkovaleria06@gmail.com>
2020-11-06 17:13:06 +0300

    NIK 1.0.0

--- Assets/Scripts/Helpp.cs
+++ Assets/Scripts/Helpp.cs
@@ -12,8 +12,8 @@ public class Helpp : MonoBehaviour
     static GameObject [] But;
     private int b = 0;
     private int j; 
-    Game game; 
-  
+    Game game;
+    Animation RuttleBones = new Animation();  
     public void Start() // сделать ход
     {
         game = new Game(); 
@@ -26,12 +26,12 @@ public class Helpp : MonoBehaviour
             But[i].transform.SetParent(Parent.transform); //Помещаем кнопку к родителю
             But[i].transform.localPosition = new Vector3(PosX, -10f, 0f); //смещаем кнопки по Х
             j = i + 1;
-            But[i].name = "B" + j.ToString(); //Дополняем кнопки нумерацией, чтобы потом можно скажем через EventSystem получать имя нажатой кнопки
-            But[i].GetComponent<Image>().sprite = Resources.Load<Sprite>("Textures/" + Board.Hand[i]); // присваиваем спрайты кнопкам  
+            But[i].name = "B" + j.ToString(); // дополняем кнопки нумерацией 
+            But[i].GetComponent<Image>().sprite = Resources.Load<Sprite>("Textures/" + Board.Hand[i]); // присваиваем спрайты кнопкам   
         }
         ButD();
         Butr();
-        Game.check.WhoseMove();
+        Game.check.WhoseMove(); 
     }
     void ButD()
     {
@@ -66,11 +66,10 @@ public class Helpp : MonoBehaviour
         }
     }
     float GameSeconds = 0;
-    float GameMinutes = 0;
-    
-
+    float GameMinutes = 0; 
     private void Update()
-    {
+    { 
+        //RuttleBones.Play();
         GameObject timetext = GameObject.Find("T_Time");
         GameSeconds += Time.deltaTime;
         timetext.GetComponent<Text>().text = (Math.Round(GameMinutes, 0) + ":" + Math.Round(GameSeconds, 0)).ToString();
@@ -82,14 +81,13 @@ public class Helpp : MonoBehaviour
         if (Moving.first == false)
         {
             Game.moving.ChangePos();
-        }
-
+        } 
     }
     public void Move1()
     {
         if (b > 0 )
         {
-            Color color = new Color(255, 255, 255, 188);
+            Color color = new Color( 1f,1f,1f,0.7f );
             Game.moving = new Moving();
             game.MakeMove();
             Game.moving.PosGoHand();
@@ -99,7 +97,7 @@ public class Helpp : MonoBehaviour
                     Game.moving.goPos[Moving.linkedList.tail.Data].GetComponent<Image>().sprite = Resources.Load<Sprite>("Textures/" + Moving.namespritebutt);
                 else
                     Game.moving.goPos[Moving.linkedList.head.Data].GetComponent<Image>().sprite = Resources.Load<Sprite>("Textures/" + Moving.namespritebutt);
-                //moving.goPos[Moving.startpos].GetComponent<Image>().color = color;
+                Game.moving.goPos[Moving.linkedList.head.Data].GetComponent<Image>().color = color;
                 Board.HandComp.RemoveAt(b);
                 Destroy(But[b-1]);
                 game.MakeMove();
@@ -111,7 +109,7 @@ public class Helpp : MonoBehaviour
                     Game.moving.goPos[Moving.linkedList.tail.Data].GetComponent<Image>().sprite = Resources.Load<Sprite>("Textures/" + Moving.namespritebutt);
                 else
                     Game.moving.goPos[Moving.linkedList.head.Data].GetComponent<Image>().sprite = Resources.Load<Sprite>("Textures/" + Moving.namespritebutt);
-                //moving.goPos[Moving.startpos].GetComponent<Image>().color = color;
+                Game.moving.goPos[Moving.linkedList.head.Data].GetComponent<Image>().color = color;
                 Board.HandComp.RemoveAt(Check.kolforCom);
                 Check.flag = false;
                 WayTrue();
