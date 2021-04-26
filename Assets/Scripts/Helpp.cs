92a40b156a9225537897caea29e2b8b43e5b04d0
DESKTOP-4G3BCO0\DomovenoKuzja <kravcenkovaleria06@gmail.com>
2021-04-26 17:56:19 +0300

    NIK 1.0.0

--- Assets/Scripts/Helpp.cs
+++ Assets/Scripts/Helpp.cs
@@ -474,9 +474,8 @@ public class Helpp : MonoBehaviour
 
         //Debug.Log(Moving.bak.Count + " " + Moving.linkedList.Count);
         //foreach (int i in masLinked)
-        //    Debug.Log(i);
+        //Debug.Log(i);
         
-
         int j;
         for (int i = 0; i < masLinked.Length; i++)
         {
@@ -484,7 +483,6 @@ public class Helpp : MonoBehaviour
             { 
                 int rotate = Rotate(masLinked[i], new Domino(Game.moving.goPos[masLinked[i]].GetComponent<Image>().sprite.name), new Domino(Game.moving.goPos[masLinked[i]].GetComponent<Image>().sprite.name));
                 Game.moving.goPos[masLinked[i]].transform.rotation = Quaternion.Euler(0, 0, rotate); 
-
             }
             else if (i >= 1)
             {
