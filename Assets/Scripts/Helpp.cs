d068006a2c90aa1d1bc1df76d1b354de13caad12
DESKTOP-4G3BCO0\DomovenoKuzja <kravcenkovaleria06@gmail.com>
2021-09-14 17:35:44 +0300

    NIK 1.0.0

--- Assets/Scripts/Helpp.cs
+++ Assets/Scripts/Helpp.cs
@@ -281,7 +281,7 @@ public class Helpp : MonoBehaviour
                 B_TakeBar.GetComponent<Button>().interactable = false;
         }
     }
-    void SpriteDomino() // кость кладется на выбранный квадрат (для последующих ходов)
+    void SpriteDomino() // кость кладется на выбранный квадрат (для последующих ходов) 
     {
         Color color = new Color(1f, 1f, 1f, 0.7f);
         if (Moving.LorR == false)
